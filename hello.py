c69933ee9e763c996c3eea0a4080f98520e9d0ce
tinchoreco <30661258+tinchoreco@users.noreply.github.com>
2023-07-17 19:51:28 -0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,37 +1,66 @@
 import streamlit as st
 import requests
-"""? """
-# Título de la página
-st.title("Clasificación de Sismos")
+import joblib
 
-# Formulario de clasificación
-st.subheader("Formulario de Clasificación")
-magnitude = st.text_input("Magnitud", value="", help="Ingrese la magnitud del sismo")
-intensity = st.text_input("Intensidad", value="", help="Ingrese la intensidad del sismo")
+def main():
+    # Título de la página
+    st.title("Clasificación de Sismos")
 
-if st.button("Clasificar"):
-    # Realizar la clasificación del sismo y mostrar el resultado
-    # en lugar de realizar una solicitud a una API externa, aquí puedes
-    
-    # Construir la URL de la API con los parámetros
-    api_url = f"https://mlapi2.onrender.com/clasificar_sismo/{magnitude}/{intensity}"
+    # Formulario de clasificación
+    st.subheader("Formulario de Clasificación")
+    magnitude = st.text_input("Magnitud", value="", help="Ingrese la magnitud del sismo")
+    intensity = st.text_input("Intensidad", value="", help="Ingrese la intensidad del sismo")
 
+    # Carga del modelo entrenado
+    model = joblib.load("kmeans_model.pkl")
 
+    # Creacion array respuesta
+    respuesta = ["""Los sismos en este rango tienen una intensidad perceptible 
+    que va desde niveles bajos hasta niveles ampliamente perceptibles en el área afectada. 
+    Pueden provocar daños menores en estructuras, 
+    como grietas en los muros y caída de revestimientos, 
+    y ser percibidos por un número variable de personas, 
+    desde unas pocas en reposo y en posición tranquila 
+    hasta todas las personas en el área afectada.""",
+    """Los sismos en este rango tienen una intensidad que generalmente no se percibe, 
+    excepto en condiciones muy favorables, 
+    hasta niveles que a menudo se perciben, 
+    pero rara vez causan daños. 
+    En términos de magnitud, 
+    van desde temblores que se sienten como vibraciones menores 
+    hasta sismos que pueden causar daños menores en estructuras.""",
+    """los sismos en este rango tienen una intensidad que generalmente no se percibe, 
+    pero que en condiciones favorables puede ser percibida por unas pocas personas en reposo 
+    y en posición tranquila. 
+    Además, la magnitud de estos sismos va desde temblores de vibración menor 
+    hasta la capacidad de causar una gran cantidad de daños en áreas habitadas.""",
+    """los sismos en este rango tienen una intensidad que a menudo se percibe 
+    y puede causar una gran cantidad de daños en áreas habitadas, 
+    junto con una magnitud que va desde temblores menores 
+    hasta la capacidad de causar daños significativos en estructuras.""",
+    """los sismos en este rango tienen una intensidad 
+    que puede causar daños significativos en áreas más grandes 
+    y una magnitud que va desde la capacidad de causar daños significativos
+    en estructuras hasta la posibilidad de ocasionar daños extensos 
+    e incluso colapso total de edificios."""]
 
-    if magnitude and intensity:
-        # Realizar la solicitud a la API y obtener la respuesta
-        response = requests.get(api_url)
-
-        # Verificar si la solicitud fue exitosa
-        if response.status_code == 200:
-            data = response.json()  # Convertir la respuesta en formato JSON a un diccionario de Python
-            # Mostrar los datos en Streamlit
-            st.write("Clasificación:", data["clasificacion"])
-            st.write(data["texto"])
-        else:
-            st.error("Error al cargar los datos de la API")
+    if st.button("Clasificar"):
+   
+        if magnitude and intensity:
             
-    else:
-        st.warning("Por favor, ingrese la magnitud y la intensidad del sismo.")
+            # Realizar la clasificación utilizando el modelo entrenado
+            clasificacion = model.predict([[magnitude, intensity]])
+
+            # Convertir el resultado en un tipo de datos nativo de Python
+            #clasificacion = np.asscalar(clasificacion)
+            clasificacion = clasificacion.item()
+    
+            # Devolver la clasificación como respuesta
+            return {"clasificacion": clasificacion, "texto": respuesta[clasificacion]}
+            
+        else:
+            st.warning("Por favor, ingrese la magnitud y la intensidad del sismo.")
 
 
+if __name__ == "__main__":
+    main()
